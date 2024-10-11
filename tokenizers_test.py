939a22993ca7a1d14ac65b440ef7171c5ac15625
Alek Dimitriev <alekdimi@users.noreply.github.com>
2024-10-10 23:17:56 -0500

    Add tokenizers_test.py

---
+++ tokenizers_test.py
@@ -0,0 +1,17 @@
+from absl.testing import absltest
+from absl.testing import parameterized
+
+import tiktoken
+import tokenizers
+
+class TokenizerTest(parameterized.TestCase):
+
+    @parameterized.parameters(("gpt2",))
+    def test_tokenizer(self, model: str):
+        tokenizer = tokenizers.TokenizerConfig(model=model).make()
+        self.assertIsInstance(tokenizer, tiktoken.Encoding)
+        tokens = tokenizer.encode("Hello, world!")
+        self.assertEqual(tokens, [15496, 11, 995, 0])
+
+if __name__ == "__main__":
+    absltest.main()
